3aef456d06ccf69c63a6c309318a227ffb3ef152
Julian Diaz Moreno <jdiazmore@uoc.edu>
2018-09-27 12:27:27 +0200

    Updated firebase config.

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,6 +1,4 @@
-//you should replace this values with yours
-
-export const environment = {
+/* export const environment = {
   production: false,
   firebase: {
     apiKey: "AIzaSyCHjrxeH8VUADKLhR5Csa54cqPRMDr4Pw8",
@@ -12,6 +10,23 @@ export const environment = {
   },
   google_web_client_id: "1092390853283-i98feg7fb1dlsm92kkcbim62855pepi8.apps.googleusercontent.com",
   facebook_app_id: 826720427470540,
+  wordpress_url: 'https://wordpress.startapplabs.com/blog/',
+  wordpress_rest_api_url: 'https://wordpress.startapplabs.com/blog/wp-json/wp/v2/'
+}; */
+
+// Firebase integration for CeamARQBCNApp project
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC5EqqrYWcILPt9Hwlf2xeNHjRolXCwv4I",
+    authDomain: "cemarqbcnapp.firebaseapp.com",
+    databaseURL: "https://cemarqbcnapp.firebaseio.com",
+    projectId: "cemarqbcnapp",
+    storageBucket: "cemarqbcnapp.appspot.com",
+    messagingSenderId: "397997714865"
+  },
+  google_web_client_id: "397997714865-k1nkuqhn4gssjnrq09j55g07372vs3gg.apps.googleusercontent.com",
+  facebook_app_id: 294884734661335,
   wordpress_url: 'https://wordpress.startapplabs.com/blog/',
   wordpress_rest_api_url: 'https://wordpress.startapplabs.com/blog/wp-json/wp/v2/'
 };
